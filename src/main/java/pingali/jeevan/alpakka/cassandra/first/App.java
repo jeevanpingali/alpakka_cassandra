c888c2b6bc4c9a4e7b6b3a4eca003a726dc1e143
Jevean Pingali <jeevan.pingali@gmail.com>
2018-08-15 18:34:26 +0530

    Clean up

--- src/main/java/pingali/jeevan/alpakka/cassandra/first/App.java
+++ src/main/java/pingali/jeevan/alpakka/cassandra/first/App.java
@@ -1,4 +1,39 @@
 package pingali.jeevan.alpakka.cassandra.first;
 
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
+import akka.stream.alpakka.cassandra.javadsl.CassandraSource;
+import akka.stream.javadsl.Sink;
+import com.datastax.driver.core.*;
+import com.datastax.driver.core.exceptions.OperationTimedOutException;
+
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
+
 public class App {
+    public static void main(String args[]) throws InterruptedException, ExecutionException, TimeoutException {
+        final Session session =
+                Cluster.builder().addContactPoint("127.0.0.1").withPort(9042).build().connect();
+
+        final ActorSystem system = ActorSystem.create();
+        final Materializer materializer = ActorMaterializer.create(system);
+
+        final Statement stmt =
+                new SimpleStatement("SELECT * FROM cycling.personal_info").setFetchSize(1);
+
+        final CompletionStage<List<Row>> rows =
+                CassandraSource.create(stmt, session).runWith(Sink.seq(), materializer);
+
+        rows.toCompletableFuture().get(3, TimeUnit.SECONDS).stream().map( r -> r.getInt("id")).collect(Collectors.toSet());
+
+        Thread.sleep(1000l);
+
+        system.terminate();
+
+    }
 }
